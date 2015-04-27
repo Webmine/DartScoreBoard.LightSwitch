b2cad76eefede126ecb5067f17e7ae4d71709bb5
heringei.daniel <heringei.daniel@webmine.hu>
2015-04-27 10:24:53 +0200

    hmm maybe we should hit save all before commit oops~

---
+++ DartScoreboard/DartScoreboard/DartScoreboard.HTMLClient/Screens/BrowseGameSet.lsml.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+
+myapp.BrowseGameSet.Id1_render = function (element, contentItem) {
+    // Write code here.
+    element.innerHTML = '<button onclick="alert('+ contentItem.__value + ')">guj</button>';
+};
---
+++ DartScoreboard/DartScoreboard/DartScoreboard.HTMLClient/Screens/DartGameScreen.lsml.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.DartGameScreen.MyCustomControl_render = function (element, contentItem) {
+element.innerHTML = '<h1>Hello World!</h1>';
+};
