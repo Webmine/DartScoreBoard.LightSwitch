c5f09e914dfb26041384f16cfe91b45118570ac4
heringei.daniel <heringei.daniel@webmine.hu>
2015-04-27 10:40:53 +0200

    removed DartGameScreen

--- DartScoreboard/DartScoreboard/DartScoreboard.HTMLClient/Screens/DartGameScreen.lsml.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
-
-myapp.DartGameScreen.MyCustomControl_render = function (element, contentItem) {
-element.innerHTML = '<h1>Hello World!</h1>';
-};
